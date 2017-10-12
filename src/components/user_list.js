6711c13189738c48e2f3ef159c308083a37afff2
torrent benoit <torrentdu64@gmail.com>
2017-10-12 15:31:30 +0200

    build list user

---
+++ src/components/user_list.js
@@ -0,0 +1,39 @@
+
+import React, { Component} from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions'
+
+
+class UserList extends Component {
+  componentWillMount() {
+    this.props.fetchUsers();
+  }
+
+
+  renderUser(user) {
+    return (
+      <div className="card card-block" >
+        <h3 className="card-title" >{user.name}</h3>
+        <p className="card-text">cheese Compagnie</p>
+        <a className="btn btn-primary">Email</a>
+
+      </div>
+      );
+
+  }
+
+  render() {
+    return (
+      <div>
+      { this.props.users.map(this.renderUser)}
+      </div>
+      );
+  }
+}
+
+function mapStateToProps(state) {
+  return { users: state.users };
+}
+
+
+export default connect(mapStateToProps, actions)(UserList);
